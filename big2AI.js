59c73457a5c77697203a496e53a4034638814646
Catherine Han <catherinehan714@gmail.com>
2018-01-24 17:59:14 -0800

    implemented 2-way play for 1x 2x 3x

--- big2AI.js
+++ big2AI.js
@@ -10,8 +10,7 @@ class Big2AI extends Big2Logic {
     this.table = {}; // same as parsedTable in selectBestHandToPlay
 
     this.selectBestHandToPlay = this.selectBestHandToPlay.bind(this);
-    this.addSingles = this.addSingles.bind(this);
-    this.addPairs = this.addPairs.bind(this);
+    this.add1x2x3x = this.add1x2x3x.bind(this);
 	}
 
 	selectBestHandToPlay(AIhand, parsedTable, yourHandLength) { 
@@ -28,47 +27,46 @@ class Big2AI extends Big2Logic {
 		if (!parsedTable) {
 			// lets assume for now that empty table = AI will play smallest single card. 
 			console.log('AI detected table is empty. ');
-      this.addSingles();
-		} else if (parsedTable.combo === '1x') {	
-      this.addSingles();
-      this.memory = this.memory.filter(hand => hand[0] > parsedTable.power);
-		} else if (parsedTable.combo === '2x') {
-      this.addPairs();
-      this.memory = this.memory.filter(hand => hand[1] > parsedTable.power); 
-		}
-
+      this.add1x2x3x(1);
+		} else if (['1x', '2x', '3x'].includes(parsedTable.combo)) {
+      console.log('AI detected combo is ', parsedTable.combo);
+      const which1x2x3x = parseInt(parsedTable.combo[0]);
+      this.add1x2x3x(which1x2x3x);
+      this.memory = this.memory.filter(hand => hand[which1x2x3x - 1] > parsedTable.power);
+    }
+    
 		console.log('AI can play: ', this.memory);
 		return this.memory[0] || [];
   }
 
-  addSingles() {
-    // 1. finish
-    this.hand.forEach(card => this.memory.push([card.big2rank]));
-  }
-  addPairs() {
-    // 1. filter out singles
-    const without_singles = this.hand.filter(card => this.rankCount(card, this.hand) >= 2);
-    console.log(without_singles);
-    // 2. partition by rank [[{}, {}], [{}, {}]]
-    const partitioned_ranks = [[without_singles[0]]];
-    for (let i = 1; i < without_singles.length; i++) {
-      if (partitioned_ranks[partitioned_ranks.length - 1][0].rank === without_singles[i].rank) {
-        partitioned_ranks[partitioned_ranks.length - 1].push(without_singles[i]);
-      } else {
-        partitioned_ranks.push([without_singles[i]]);
+  add1x2x3x(x) {
+    // add singles, pairs, or triplets to memory (x = 1, 2, or 3)
+    if (x === 1) {
+      this.hand.forEach(card => this.memory.push([card.big2rank]));
+    } else {
+      // 1. filter out singles, pairs, if necessary
+      const filter_unplayables = this.hand.filter(card => this.rankCount(card, this.hand) >= x);
+      //console.log(filter_unplayables);
+      // 2. partition by rank [[{}, {}], [{}, {}]]
+      const partition_ranks = [[filter_unplayables[0]]];
+      for (let i = 1; i < filter_unplayables.length; i++) {
+        if (partition_ranks[partition_ranks.length - 1][0].rank === filter_unplayables[i].rank) {
+          partition_ranks[partition_ranks.length - 1].push(filter_unplayables[i]);
+        } else {
+          partition_ranks.push([filter_unplayables[i]]);
+        }
       }
+      //console.log(partition_ranks);
+      // 3. add all perms in each rank division
+      let all_pairs = [];
+      partition_ranks.forEach(partition => {
+        all_pairs = all_pairs.concat(this.allCombinations(partition, x));
+      });
+      //console.log(all_pairs);
+      // 4. finish
+      all_pairs.forEach(pair => this.memory.push(pair.map(card => card.big2rank)));
     }
-    console.log(partitioned_ranks);
-    // 3. add all perms in each rank division
-    let all_pairs = [];
-    partitioned_ranks.forEach(partition => {
-      all_pairs = all_pairs.concat(this.allCombinations(partition, 2));
-    });
-    console.log(all_pairs);
-    // 4. finish
-    all_pairs.forEach(pair => this.memory.push(pair.map(card => card.big2rank)));
   }
-
 };
 
 /* POSSIBLE HANDS
